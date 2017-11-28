8a77933b4a6f0d813930af48815dfe4343680991
hesamaccount <hesamdarbandi@hotmail.com>
2017-11-28 08:37:38 -0800

    create TimeControl

---
+++ Plugins/TimeControl.ts
@@ -0,0 +1,28 @@
+
+///<reference path="../require.config.ts"/>
+
+import helper = require("/Component/Helper");
+
+export namespace Olive {
+   export class TimeControl{
+       input:any;
+       
+       constructor(targetInput:any){
+           this.input=targetInput;
+       }
+       
+        if (helper.getInstance().isWindowModal()) {
+            input.off("dp.show.adjustHeight").on("dp.show.adjustHeight", (e) => this.adjustModalHeightForDataPicker(e));
+            input.off("dp.hide.adjustHeight").on("dp.hide.adjustHeight", (e) => this.adjustModalHeightForDataPicker(e));
+        }
+        input.attr("data-autofocus", "disabled");
+        input.datetimepicker({
+            format: this.TIME_FORMAT,
+            useCurrent: false,
+            stepping: parseInt(input.attr("data-minute-steps") || this.MINUTE_INTERVALS.toString()),
+            keepInvalid: input.closest("form").find("[data-change-action]").length == 0,
+            locale: this.DATE_LOCALE
+        }).data("DateTimePicker").keyBinds().clear = null;
+        input.parent().find(".fa-clock-o").parent(".input-group-addon").click(() => { input.focus(); });
+   }
+}
