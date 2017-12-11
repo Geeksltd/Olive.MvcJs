58f1a614e08791334f39f2400cd64ad96183f944
hesamaccount <hesamdarbandi@hotmail.com>
2017-12-11 06:26:56 -0800

    tested version of DateAndTime Plugin

--- Plugins/TimeControl.js
+++ Plugins/TimeControl.js
@@ -1,26 +1,33 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var WindowContext_1 = require("../Component/WindowContext");
-var Config_1 = require("../Config");
-var TimeControl = (function () {
-    function TimeControl(targetInput) {
-        var input = targetInput;
-        var windowCtx = WindowContext_1.WindowContext.getInstance();
-        if (windowCtx.isWindowModal()) {
-            input.off("dp.show.adjustHeight").on("dp.show.adjustHeight", function (e) { return windowCtx.adjustModalHeightForDataPicker(e); });
-            input.off("dp.hide.adjustHeight").on("dp.hide.adjustHeight", function (e) { return windowCtx.adjustModalHeightForDataPicker(e); });
-        }
-        input.attr("data-autofocus", "disabled");
-        input.datetimepicker({
-            format: Config_1.Config.TIME_FORMAT,
-            useCurrent: false,
-            stepping: parseInt(input.attr("data-minute-steps") || Config_1.Config.MINUTE_INTERVALS.toString()),
-            keepInvalid: input.closest("form").find("[data-change-action]").length == 0,
-            locale: Config_1.Config.DATE_LOCALE
-        }).data("DateTimePicker").keyBinds().clear = null;
-        input.parent().find(".fa-clock-o").parent(".input-group-addon").click(function () { input.focus(); });
-    }
-    return TimeControl;
-}());
-exports.TimeControl = TimeControl;
-//# sourceMappingURL=TimeControl.js.map
+
+
+import { WindowContext as windowContext } from "../components/windowContext"
+
+export namespace Olive {
+       export class TimeControl {
+           input:any;
+           
+           constructor(targetInput:any) {
+               this.input = targetInput;
+           }
+
+           show() {
+
+               if (windowContext.isWindowModal()) {
+                   this.input.off("dp.show.adjustHeight").on("dp.show.adjustHeight", (e) => windowContext.adjustModalHeightForDataPicker(e));
+                   this.input.off("dp.hide.adjustHeight").on("dp.hide.adjustHeight", (e) => windowContext.adjustModalHeightForDataPicker(e));
+               }
+
+               this.input.attr("data-autofocus", "disabled");
+               this.input.datetimepicker({
+                   format: windowContext.setting.TIME_FORMAT,
+                    useCurrent: false,
+                    stepping: parseInt(this.input.attr("data-minute-steps") || windowContext.setting.MINUTE_INTERVALS.toString()),
+                    keepInvalid: this.input.closest("form").find("[data-change-action]").length == 0,
+                    locale: windowContext.setting.DATE_LOCALE
+               }).data("DateTimePicker").keyBinds().clear = null;
+
+               var targetEventSelector = this.input.attr("data-control") == "time-picker" ? ".fa-clock-o" : ".fa-calendar";
+               this.input.parent().find(targetEventSelector).parent(".input-group-addon").click(() => { this.input.focus(); });
+           }
+       }
+}
