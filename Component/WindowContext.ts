5ab4e3c867a544147656a7def432989ce9f1d105
hesamaccount <hesamdarbandi@hotmail.com>
2017-12-11 07:30:55 -0800

    Add autoComplete Dependency method

--- Component/WindowContext.ts
+++ Component/WindowContext.ts
@@ -1,3 +1,6 @@
+///<reference path="../application.urlhelper.ts"/>
+///<reference path="../typings/jquery/index.d.ts"/>
+
 import * as $ from '../../../lib/jquery/dist/jquery.js'
     export class WindowContext {
 
@@ -36,8 +39,45 @@ import * as $ from '../../../lib/jquery/dist/jquery.js'
                 if (frame.attr("data-has-explicit-height") != 'true')
                     frame.height(document.body.offsetHeight + (overflow || 0));
             }
-        }
+            public static getPostData(trigger: JQuery): JQuerySerializeArrayElement[] {
+           var form = trigger.closest("[data-module]");
+           if (!form.is("form")) form = $("<form />").append(form.clone(true));
+           var data = urlHelper.mergeFormData(form.serializeArray());
+           // If it's master-details, then we need the index.
+           var subFormContainer = trigger.closest(".subform-item");
+           if (subFormContainer != null) {
+               data.push({
+                   name: "subFormIndex",
+                   value: subFormContainer.closest(".horizontal-subform, .vertical-subform").find(".subform-item").index(subFormContainer).toString()
+               });
+           }
+
+           data.push({ name: "current.request.url", value: urlHelper.pathAndQuery() });
+           return data;
+       }
+
+       public static handleAjaxResponseError(response) {
+           this.hidePleaseWait();
+           console.log(response);
+
+           var text = response.responseText;
+           if (text.indexOf("<html") > -1) {
+               document.write(text);
+           }
+           else if (text.indexOf("<form") > -1) {
+               var form = $("form", document);
+               if (form.length) form.replaceWith($(text));
+               else document.write(text);
+           }
+           else alert(text);
+       }
+
+       public static hidePleaseWait() {
+           $(".wait-screen").remove();
+       }
+           
     }
+ }
 
     
 
