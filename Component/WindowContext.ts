66ba2ce05b6b34fc333c366463e7f29df1e331a1
hesamaccount <hesamdarbandi@hotmail.com>
2017-12-11 06:31:43 -0800

    tested version of WindowContext

--- Component/WindowContext.ts
+++ Component/WindowContext.ts
@@ -1,45 +1,43 @@
+import * as $ from '../../../lib/jquery/dist/jquery.js'
+    export class WindowContext {
 
-// TODO: Change these to be extension methods on the Window interface.
-// So for example we can call -> window.getContainerIFrame() or window.isModal()...
+        static setting = {
+            TIME_FORMAT: "HH:mm",
+            MINUTE_INTERVALS : 5,
+            DATE_LOCALE:"en-gb"
+        };
 
-export class WindowContext {
-    private static instance: WindowContext;
+        public static isWindowModal(): boolean {
+            if ($(this.getContainerIFrame()).closest(".modal").length === 0) return false;
+            return true;
+        }
 
-    static getInstance() {
-        if (!WindowContext.instance) WindowContext.instance = new WindowContext();
-        return WindowContext.instance;
-    }
+        public static getContainerIFrame() {
+            if (parent == null || parent == self) return null;
+            return $(parent.document).find("iframe").filter((i, f: any) => (f.contentDocument || f.contentWindow.document) == document).get(0);
+        }
 
-    public isWindowModal() {
-        if ($(this.getContainerIFrame()).closest(".modal").length === 0) return false;
-        return true;
-    }
+        public static adjustModalHeightForDataPicker(target: any) {
+            var datepicker = $(target.currentTarget).siblings('.bootstrap-datetimepicker-widget');
 
-    public getContainerIFrame() {
-        if (parent == null || parent == self) return null;
-        return $(parent.document).find("iframe").filter((i, f: any) => (f.contentDocument || f.contentWindow.document) == document).get(0);
-    }
+            if (datepicker.length === 0) {
+                this.adjustModalHeight();
+                return;
+            }
 
-    public adjustModalHeight(overflow?: number) {
-        if (this.isWindowModal()) {
+            var offset = Math.ceil(datepicker.offset().top + datepicker[0].offsetHeight) - document.body.offsetHeight + 6;
+            var overflow = Math.max(offset, 0);
+            this.adjustModalHeight(overflow);
+        }
 
-            var frame = $(this.getContainerIFrame());
-            if (frame.attr("data-has-explicit-height") != 'true')
-                frame.height(document.body.offsetHeight + (overflow || 0));
+       public static adjustModalHeight(overflow?: number) {
+            if (this.isWindowModal()) {
+                var frame = $(this.getContainerIFrame());
+                if (frame.attr("data-has-explicit-height") != 'true')
+                    frame.height(document.body.offsetHeight + (overflow || 0));
+            }
         }
     }
 
-    public adjustModalHeightForDataPicker(e) {
+    
 
-        var datepicker = $(e.currentTarget).siblings('.bootstrap-datetimepicker-widget');
-
-        if (datepicker.length === 0) {
-            this.adjustModalHeight();
-            return;
-        }
-
-        var offset = Math.ceil(datepicker.offset().top + datepicker[0].offsetHeight) - document.body.offsetHeight + 6;
-        var overflow = Math.max(offset, 0);
-        this.adjustModalHeight(overflow);
-    }   
-}
