2ba559e2197177f0426eae30af3f8cc857c1475b
hesamaccount <hesamdarbandi@hotmail.com>
2017-12-12 06:40:18 -0800

    create datePicker plugin

---
+++ Plugins/datePicker.ts
@@ -0,0 +1,42 @@
+
+import { WindowContext } from "../Components/WindowContext"
+import { Config } from "../Config"
+
+  export class DatePicker {
+           input:any;
+           
+           constructor(targetInput:any) {
+               this.input = targetInput;
+           }
+
+           show() {
+
+               if (WindowContext.isWindowModal()) {
+                   this.input.off("dp.show.adjustHeight").on("dp.show.adjustHeight", (e) => WindowContext.adjustModalHeightForDataPicker(e));
+                   this.input.off("dp.hide.adjustHeight").on("dp.hide.adjustHeight", (e) => WindowContext.adjustModalHeightForDataPicker(e));
+               }
+
+               this.input.attr("data-autofocus", "disabled");
+               var control = this.input.attr("data-control");
+               var viewMode = this.input.attr("data-view-mode") || 'days';
+
+               if (control == "date-picker") {
+                   (<any>this.input).datetimepicker({
+                       format: Config.DATE_FORMAT,
+                       useCurrent: false,
+                       showTodayButton: true,
+                       icons: { today: 'today' },
+                       viewMode: viewMode,
+                       keepInvalid: this.input.closest("form").find("[data-change-action]").length == 0,
+                       locale: Config.DATE_LOCALE
+                   }).data("DateTimePicker").keyBinds().clear = null;
+                   // Now make calendar icon clickable as well             
+                   this.input.parent().find(".fa-calendar").parent(".input-group-addon").click(() => { this.input.focus(); });
+               }
+               else alert("Don't know how to handle date control of " + control);
+           }
+   }
+
+
+
+
