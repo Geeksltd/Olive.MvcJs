9776416e45a51d506e8676ef0087422ae456851e
hesamaccount <hesamdarbandi@hotmail.com>
2017-12-15 01:21:43 -0800

    move updateSubFormStates to windowContext

--- src/Components/WindowContext.ts
+++ src/Components/WindowContext.ts
@@ -95,6 +95,26 @@ export class WindowContext {
         $("<input type='checkbox' checked='checked'/>").hide().attr("name", checkboxes.attr("name")).val("-")
             .appendTo(button.parent());
      }
+
+    public static updateSubFormStates() {
+        var countItems = (element) => $(element).parent().find(".subform-item:visible").length;
+        // Hide removed items
+        $("input[name*=MustBeDeleted][value=True]").closest('[data-subform]').hide();
+        // hide empty headers
+        $(".horizontal-subform thead").each((i, e) => {
+            $(e).css('visibility', (countItems(e) > 0) ? 'visible' : 'hidden');
+        });
+        // Hide add buttons
+        $("[data-subform-max]").each((i, e) => {
+            var show = countItems(e) < parseInt($(e).attr('data-subform-max'));
+            $(e).find("[data-add-subform=" + $(e).attr("data-subform") + "]").toggle(show);
+        });
+        // Hide delete buttons
+        $("[data-subform-min]").each((i, e) => {
+            var show = countItems(e) > parseInt($(e).attr('data-subform-min'));
+            $(e).find("[data-delete-subform=" + $(e).attr("data-subform") + "]").css('visibility', (show) ? 'visible' : 'hidden');
+        });
+     }
 }
 
 
