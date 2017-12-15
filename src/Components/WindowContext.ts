2afcc3a54734aeee31b67bab2bc02acdc1bc37b5
hesamaccount <hesamdarbandi@hotmail.com>
2017-12-15 03:35:39 -0800

    move showPleaseWait to context

--- src/Components/WindowContext.ts
+++ src/Components/WindowContext.ts
@@ -317,6 +317,22 @@ public static alertUnobtrusively(message: string, style?: string) {
             console.log("Unable to replace list items");
         }
     }
+
+    public static showPleaseWait(blockScreen: boolean = false) {
+        if (!$(document.forms[0]).valid()) return;
+
+        var screen = $("<div class='wait-screen' />").appendTo("body");
+        if (blockScreen) {
+            $("<div class='cover' />")
+                .width(Math.max($(document).width(), $(window).width()))
+                .height(Math.max($(document).height(), $(window).height()))
+                .appendTo(screen);
+        }
+
+        $("<div class='wait-container'><div class='wait-box'><img src='/public/img/loading.gif'/></div>")
+            .appendTo(screen)
+            .fadeIn('slow');
+    }
 }
 
 
