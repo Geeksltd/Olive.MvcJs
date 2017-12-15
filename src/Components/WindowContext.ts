5d3cdecae2b63443517293a70da92a95d2e56297
hesamaccount <hesamdarbandi@hotmail.com>
2017-12-15 01:43:15 -0800

    move enableAjaxPaging to context

--- src/Components/WindowContext.ts
+++ src/Components/WindowContext.ts
@@ -176,6 +176,22 @@ export class WindowContext {
     public static paginationSizeChanged(event: Event) {
         $(event.currentTarget).closest("form").submit();
     }
+
+    public static enableAjaxPaging(event: JQueryEventObject) {
+        var button = $(event.currentTarget);
+        var page = button.attr("data-pagination");
+        var key = "p";
+
+        if (page.split('=').length > 1) { key = page.split('=')[0]; page = page.split('=')[1]; }
+
+        var input = $("[name='" + key + "']");
+        input.val(page);
+        if (input.val() != page) {
+            // Drop down list case
+            input.parent().append($("<input type='hidden'/>").attr("name", key).val(page));
+            input.remove();
+        }
+    }
 }
 
 
