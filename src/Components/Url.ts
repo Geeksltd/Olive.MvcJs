c31932f2b48134cc3491cdddbf5a944138cedce7
hesamaccount <hesamdarbandi@hotmail.com>
2017-12-19 08:47:50 -0800

    improve the code using tips

--- src/Components/Url.ts
+++ src/Components/Url.ts
@@ -3,7 +3,7 @@
     static current(): string { return window.location.href; }
 
     static goBack(): void {
-        var returnUrl = Url.getQuery("ReturnUrl");
+        let returnUrl = Url.getQuery("ReturnUrl");
         if (returnUrl) window.location.href = returnUrl;
         else history.back();
     }
@@ -11,21 +11,21 @@
     static updateQuery(uri, key, value) {
         if (uri == null) uri = window.location.href;
 
-        var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-        var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+        let re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+        let separator = uri.indexOf('?') !== -1 ? "&" : "?";
         if (uri.match(re)) return uri.replace(re, '$1' + key + "=" + value + '$2');
         else return uri + separator + key + "=" + value;
     }
 
     static removeQuery(url: string, parameter: string) {
         //prefer to use l.search if you have a location/link object
-        var urlParts = url.split('?');
+        let urlParts = url.split('?');
         if (urlParts.length >= 2) {
-            var prefix = encodeURIComponent(parameter).toLowerCase() + '=';
-            var parts = urlParts[1].split(/[&;]/g);
+            let prefix = encodeURIComponent(parameter).toLowerCase() + '=';
+            let parts = urlParts[1].split(/[&;]/g);
 
             //reverse iteration as may be destructive
-            for (var i = parts.length; i-- > 0;) {
+            for (let i = parts.length; i-- > 0;) {
                 //idiom for string.startsWith
                 if (parts[i].toLowerCase().lastIndexOf(prefix, 0) !== -1) {
                     parts.splice(i, 1);
@@ -43,7 +43,7 @@
         if (url) url = Url.fullQueryString(url); else url = location.search;
 
         name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)", "i"),
+        let regex = new RegExp("[\\?&]" + name + "=([^&#]*)", "i"),
             results = regex.exec(url);
         return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
     }
@@ -61,12 +61,12 @@
 
     static removeEmptyQueries(url: string): string {
 
-        var items = Url.fullQueryString(url).split('&');
-        var result = '';
+        let items = Url.fullQueryString(url).split('&');
+        let result = '';
 
-        for (var i in items) {
-            var key = items[i].split('=')[0];
-            var val = items[i].split('=')[1];
+        for (let i in items) {
+            let key = items[i].split('=')[0];
+            let val = items[i].split('=')[1];
             if (val != '' && val != undefined) result += "&" + key + "=" + val;
         }
 
