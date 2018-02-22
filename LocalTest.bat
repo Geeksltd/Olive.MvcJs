c63a3b8968c235266ee356f4042a6aef9e97f20d
PaymonK <35069374+PaymonK@users.noreply.github.com>
2018-02-22 14:53:48 +0000

    Delete LocalTest.bat

--- LocalTest.bat
+++
@@ -1,4 +0,0 @@
-
-xcopy /y /s dist\*.* C:\Projects\Olive.Templates\MvcFull\Template\Website\wwwroot\lib\olive.mvc\dist\
-xcopy /y /s src\*.* C:\Projects\Olive.Templates\MvcFull\Template\Website\wwwroot\lib\olive.mvc\src\
-
