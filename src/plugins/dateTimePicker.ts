0d590965ab0da47eb3e6f415bf8cd989adc8e4a2
behrangmohseni <behrangmohseni@live.com>
2018-03-12 16:24:25 +0330

    no message

--- src/plugins/dateTimePicker.ts
+++ src/plugins/dateTimePicker.ts
@@ -1,25 +1,47 @@
-﻿import Modal from "olive/components/modal"
-import Config from "olive/config";
+﻿
+import Modal from "olive/components/modal"
+import Config from "olive/config"
+import DatePicker from "olive/plugins/datePicker";
+import TimeControl from "olive/plugins/timeControl";
 
 export default class DateTimePicker {
-    public static enable(selector: JQuery) { selector.each((i, e) => new DateTimePicker($(e))); }
+    input: any;
+
+    public static enable(selector: JQuery) { selector.each((i, e) => new DateTimePicker($(e)).show()); }
 
     constructor(targetInput: any) {
-        let input = targetInput;
+        this.input = targetInput;
+    }
+
+    show() {
 
         if (window.isModal()) {
-            input.off("dp.show.adjustHeight").on("dp.show.adjustHeight", e => Modal.expandToFitPicker(e));
-            input.off("dp.hide.adjustHeight").on("dp.hide.adjustHeight", e => Modal.expandToFitPicker(e));
+            this.input.off("dp.show.adjustHeight").on("dp.show.adjustHeight", e => Modal.expandToFitPicker(e));
+            this.input.off("dp.hide.adjustHeight").on("dp.hide.adjustHeight", e => Modal.expandToFitPicker(e));
         }
 
-        input.attr("data-autofocus", "disabled");
-        input.datetimepicker({
-            format: Config.DATE_FORMAT,
-            useCurrent: false,
-            keepInvalid: input.closest("form").find("[data-change-action]").length == 0,
-            locale: Config.DATE_LOCALE
-        }).data("DateTimePicker").keyBinds().clear = null;
+        this.input.attr("data-autofocus", "disabled");
+        let control = this.input.attr("data-control");
+        let viewMode = this.input.attr("data-view-mode") || 'days';
 
-        input.parent().find(".fa-clock-o").parent(".input-group-addon").click(() => { input.focus(); });
+        if (control == "date-picker|time-picker") {
+            (<any>this.input).datetimepicker({
+                format: Config.DATE_TIME_FORMAT,
+                useCurrent: false,
+                showTodayButton: true,
+                icons: { today: 'Date' },
+                viewMode: viewMode,
+                keepInvalid: this.input.closest("form").find("[data-change-action]").length == 0,
+                locale: Config.DATE_LOCALE
+            }).data("DateTimePicker").keyBinds().clear = null;
+            // Now make calendar icon clickable as well             
+            this.input.parent().find(".fa-calendar").parent(".input-group-addon").click(() => this.input.focus());
+        }
+        else alert("Don't know how to handle date control of " + control);
     }
 }
+
+
+
+
+
