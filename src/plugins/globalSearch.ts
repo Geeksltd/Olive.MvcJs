3e9ba0c27341bd7c904d6dcd3550666a54be5b99
Mohsen <mohsens2@outlook.com>
2018-06-07 22:16:34 +0430

    ditched those two console logs

--- src/plugins/globalSearch.ts
+++ src/plugins/globalSearch.ts
@@ -19,10 +19,6 @@ export default class GlobalSearch {
         this.input.wrap("<div class='typeahead__container'></div>");
 
         this.valueField = $("[name='" + this.input.attr("name").slice(0, -5) + "']");
-        if (this.valueField.length === 0) {
-            console.log("Failed to find the value field for global-search:");
-            console.log(this.input);
-        }
 
         
         let urlsList = (<string>this.input.attr("data-search-source") || '').split(";");
