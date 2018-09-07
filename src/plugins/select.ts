e07cf3341605a8b79f4f8eb1457abc34332de4f7
MRP <ipazooki@gmail.com>
2018-09-07 17:47:06 +0430

    improve select

--- src/plugins/select.ts
+++ src/plugins/select.ts
@@ -5,9 +5,7 @@
     static enhance(selectControl: JQuery) {
         selectControl.chosen({ disable_search_threshold: 5, width: "100%" });
         //this fix chosen issue with jQuery validation (https://github.com/harvesthq/chosen/issues/515#issuecomment-33214050)
-        $('select').each(function (index) {
-            this.setAttribute('style', 'display:visible; position:absolute; clip:rect(0,0,0,0)');
-        });
+        selectControl.attr("style", "display:visible; position:absolute; clip:rect(0,0,0,0)");
     }
 
     public static replaceSource(controlId: string, items) {
