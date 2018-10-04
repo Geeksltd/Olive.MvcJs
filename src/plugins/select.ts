59d9eb26f7f69bc3c865cbfe527b0a6cc43cf735
hamid_mayeli_geeksltd <hamid.mayeli@geeks.ltd.uk>
2018-10-04 10:06:00 +0100

    Add a condition to check whether the browser supported by 'chosen' or not.

--- src/plugins/select.ts
+++ src/plugins/select.ts
@@ -1,15 +1,17 @@
 ﻿export default class Select {
-     
-    public static enableEnhance(selector:JQuery){selector.each((i,e)=> this.enhance($(e)));}
-     
+
+    public static enableEnhance(selector: JQuery) { selector.each((i, e) => this.enhance($(e))); }
+
     static enhance(selectControl: JQuery) {
         selectControl.chosen({ disable_search_threshold: 5, width: "100%" });
         //this fix chosen issue with jQuery validation (https://github.com/harvesthq/chosen/issues/515#issuecomment-55901946)
-        selectControl.next(".chosen-container").prepend(selectControl.detach());
-        selectControl.attr("style", "display:visible; position:absolute; clip:rect(0,0,0,0)");
-        selectControl.on("click focus keyup change", () => {
-            selectControl.trigger("blur");
-        });
+        if (selectControl.css('display') === 'none') {
+            selectControl.next(".chosen-container").prepend(selectControl.detach());
+            selectControl.attr("style", "display:visible; position:absolute; clip:rect(0,0,0,0)");
+            selectControl.on("click focus keyup change", () => {
+                selectControl.trigger("blur");
+            });
+        }
     }
 
     public static replaceSource(controlId: string, items) {
