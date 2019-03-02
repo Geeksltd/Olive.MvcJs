4766889fe7ffb8fb852e81d0194fd03c63ca8760
alireza tabatabaee <sa.tabatabaee@gmail.com>
2019-03-02 13:36:09 +0330

    "userHelp" plugin : convert message to Html

--- src/plugins/userHelp.ts
+++ src/plugins/userHelp.ts
@@ -6,7 +6,7 @@ export default class UserHelp {
     constructor(targetElement: JQuery) {
         this.element = targetElement;
         this.element.click(() => false);
-        let message = this.element.attr('data-user-help');  // todo: unescape message and conver to html
-        this.element['popover']({ trigger: 'focus', content: message });
+        let message = this.element.attr('data-user-help');
+        this.element['popover']({ trigger: 'focus', content: message, html: true });
     }
 }
