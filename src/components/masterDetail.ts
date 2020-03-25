688af90c302886958ff7cbeb579ec8899238b780
HamidTheGeek <hamid.mayeli@yahoo.com>
2020-03-25 17:20:36 +0000

    Fix a validation issue.

--- src/components/masterDetail.ts
+++ src/components/masterDetail.ts
@@ -9,35 +9,39 @@ export default class MasterDetail implements IService {
         this.responseProcessor.subformChanged.handle((_) => this.updateSubFormStates());
     }
 
-    public enable(selector: JQuery) { selector.off("click.delete-subform").on("click.delete-subform", (e) => this.deleteSubForm(e)); }
+    public enable(selector: JQuery) {
+        selector.off("click.delete-subform").on("click.delete-subform", (e) => this.deleteSubForm(e));
+    }
 
     public updateSubFormStates() {
-        let countItems = element => $(element).parent().find(".subform-item:visible").length;
+        const countItems = (element) => $(element).parent().find(".subform-item:visible").length;
         // Hide removed items
         $("input[name$=MustBeDeleted][value=False]").val("false");
         $("input[name$=MustBeDeleted][value=True]").val("true");
-        $("input[name$=MustBeDeleted][value=true]").closest('.subform-item').hide();
+        $("input[name$=MustBeDeleted][value=true]").closest(".subform-item").hide();
         // hide empty headers
         $(".horizontal-subform thead").each((i, e) =>
-            $(e).css('visibility', (countItems(e) > 0) ? 'visible' : 'hidden'));
+            $(e).css("visibility", (countItems(e) > 0) ? "visible" : "hidden"));
         // Hide add buttons
         $("[data-subform-max]").each((i, e) => {
-            let show = countItems(e) < parseInt($(e).attr('data-subform-max'));
-            $(e).closest('[data-module]').find("[data-add-subform=" + $(e).attr("data-subform") + "]").toggle(show);
+            const show = countItems(e) < parseInt($(e).attr("data-subform-max"), 10);
+            $(e).closest("[data-module]").find("[data-add-subform=" + $(e).attr("data-subform") + "]").toggle(show);
         });
         // Hide delete buttons
         $("[data-subform-min]").each((i, e) => {
-            let show = countItems(e) > parseInt($(e).attr('data-subform-min'));
-            $(e).find("[data-delete-subform=" + $(e).attr("data-subform") + "]").css('visibility', (show) ? 'visible' : 'hidden');
+            const show = countItems(e) > parseInt($(e).attr("data-subform-min"), 10);
+            $(e).find("[data-delete-subform=" + $(e).attr("data-subform") + "]").css("visibility", (show) ? "visible" : "hidden");
         });
     }
 
     private deleteSubForm(event: JQueryEventObject) {
-        let button = $(event.currentTarget);
+        const button = $(event.currentTarget);
 
-        let container = button.parents(".subform-item");
+        const container = button.parents(".subform-item");
         this.validate.removeTooltipsRelatedTo(container);
         container.find("input[name$=MustBeDeleted]").val("true");
+        container.find("input[data-val=true]").data("val", false);
+        container.find("input[data-val=true]").attr("data-val", "false");
         this.updateSubFormStates();
         event.preventDefault();
     }
