1ade8cfd1e1e07867ef7b573e7347a2a65c6e421
AminShokrzade1987117 <amin.shokrzade@geeks.ltd.uk>
2022-09-02 14:58:25 +0100

    =

--- src/plugins/userHelp.ts
+++ src/plugins/userHelp.ts
@@ -5,7 +5,8 @@ export default class UserHelp {
         element.click(() => false);
         let message = element.attr('data-user-help');
         element['popover']({ trigger: 'focus', content: message, html: true });
-        var inputsibling = element.siblings("input");
-        inputsibling['popover']({ trigger: 'focus', content: message, html: true });
+        var inputsibling = element.parent().prev('[type=text]');
+        if (inputsibling != undefined && inputsibling != null && inputsibling.length > 0)
+            inputsibling['popover']({ trigger: 'focus', content: message, html: true, placement: 'top' });
     }
 }
